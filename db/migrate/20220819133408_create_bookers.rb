409f0cf15f74766b02845b70fc955b310a4347e3
EC2 Default User <ec2-user@ip-172-31-44-111.ap-northeast-1.compute.internal>
2022-08-21 03:53:41 +0000

    [Start]Bookers

---
+++ db/migrate/20220819133408_create_bookers.rb
@@ -0,0 +1,11 @@
+class CreateBookers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookers do |t|
+      
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
