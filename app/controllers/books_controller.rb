bd950f39158ed03a62d56e8b64dc21006d516739
EC2 Default User <ec2-user@ip-172-31-44-111.ap-northeast-1.compute.internal>
2022-08-27 12:33:41 +0000

    [Update]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
-class BookersController < ApplicationController
+class BooksController < ApplicationController
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path(book.id)
+  end
+
+  def show
+    @book =Book.find(params[:id])
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_19_133408) do
+ActiveRecord::Schema.define(version: 2022_08_24_121010) do
 
-  create_table "bookers", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
