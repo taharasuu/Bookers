db837785792b5a3cd1fbe2bf0e9440f3ee3afbaf
EC2 Default User <ec2-user@ip-172-31-44-111.ap-northeast-1.compute.internal>
2022-08-30 10:19:41 +0000

    [Finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,14 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(book.id)
+    else
+      @book = book
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -16,6 +22,27 @@ class BooksController < ApplicationController
   end
   
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    if book.save
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(book.id)
+    else
+      @book = book
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    book.save
+    flash[:notice] = "Book was successfully destroyed"
+    redirect_to books_path
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   
   root to: 'homes#top'
   resources :books
+  patch 'edit_book_path/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  class Book<ApplicationRecord
+    validates :title, presence: true
+    validates :body, presence: true
+  end
 end
